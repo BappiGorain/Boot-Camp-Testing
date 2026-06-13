6565e8518512ae130063b958d1a46b85631ef8d0
BappiGorain <146017226+BappiGorain@users.noreply.github.com>
2026-06-13 20:57:00 +0530

    13-06-26

---
+++ Explicit Wait Demo/src/com/explict/wait/ExplictWaitDemo.java
@@ -0,0 +1,26 @@
+package com.explict.wait;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplictWaitDemo
+{
+	public static void main(String args[]) throws Exception
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://google.com");
+		
+		driver.findElement(By.name("q")).sendKeys("Selelenium Automation Testing");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.name("Apj")));
+		element.click();
+	}
+}
--- Implecit Wait/src/com/implicit/wait/ImplicitWaitDemo.java
+++ Implecit Wait/src/com/implicit/wait/ImplicitWaitDemo.java
@@ -15,8 +15,8 @@ public class ImplicitWaitDemo
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.easemytrip.com");
-		driver.findElement(By.xpath("//input[@id='FromSector_show']")).sendKeys("Hyderabad",Keys.ENTER);
-		driver.findElement(By.xpath("//input[@id='FromSector_show']")).sendKeys("bhubaneswar",Keys.ENTER);
+		driver.findElement(By.xpath("//input[@id='FromSector_show']")).sendKeys("Delhi",Keys.ENTER);
+		driver.findElement(By.xpath("//input[@id='FromSector_show']")).sendKeys("Mumbai",Keys.ENTER);
 		driver.findElement(By.xpath("//input[@id='ddate']")).click();
 		driver.findElement(By.id("trd_6_19/12/2026")).click();
 		driver.findElement(By.xpath("//button[normalize-space()='Search']")).click();
